70d4de3add519e9c0d66cc6b871c29d2fa34c4cc
lucastrschneider <lucastrschneider@gmail.com>
2020-11-03 12:13:12 -0300

    :recycle: Refactor sign extend

--- EP5/component/signExtend.vhd
+++ EP5/component/signExtend.vhd
@@ -4,7 +4,7 @@
 --! @author Lucas Schneider (lucastrschneider@usp.br)
 --! @date 2020-11-03
 
---! Last submission: #8714
+--! Last submission: #8719
 -------------------------------------------------------
 
 library ieee;
@@ -22,10 +22,10 @@ architecture combinational of signExtend is
 begin
 
     with i(31 downto 27) select
-        address <= bit_vector(to_signed(to_integer(signed(i(20 downto 12))), 64)) when "11111", -- Type D
-                bit_vector(to_signed(to_integer(signed(i(23 downto 5))), 64)) when "10110", -- Type CB
-                bit_vector(to_signed(to_integer(signed(i(25 downto 0))), 64)) when "00010", -- Type B
-                bit_vector(to_signed(0, 64)) when others;
+        address <=  bit_vector(resize(signed(i(20 downto 12)), 64)) when "11111", -- Type D
+                    bit_vector(resize(signed(i(23 downto  5)), 64)) when "10110", -- Type CB
+                    bit_vector(resize(signed(i(25 downto  0)), 64)) when "00010", -- Type B
+                    bit_vector(to_signed(0, 64)) when others;
 
     o <= address;
 
